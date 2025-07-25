9751f84efd34c4b670ce9f800d7b08c7fd50b0e6
AlexeerCT <alexeergamer@gmail.com>
2025-07-25 07:09:05 -0600

    refactor(router): Reorder inclusion of gusuarios_router in API router

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -3,6 +3,6 @@ from fastapi import APIRouter
 from .modules import gusuarios_router, gaaduanal_router, spartes_router
 
 api_router = APIRouter()
-api_router.include_router(gusuarios_router, prefix="/gusuarios", tags=["GUsuarios"])
 api_router.include_router(gaaduanal_router, prefix="/gaaduanal", tags=["GAAduanal"])
+api_router.include_router(gusuarios_router, prefix="/gusuarios", tags=["GUsuarios"])
 api_router.include_router(spartes_router, prefix="/spartes", tags=["SPartes"])
